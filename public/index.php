d014d4d8cc7794814e6251b4a930945c7432e6d8
Matthieu <matthieudejean@hotmail.com>
2021-04-11 22:32:46 +0200

    autoload ehime done

--- public/index.php
+++ public/index.php
@@ -3,4 +3,11 @@
 require_once '../src/Wcs/Hello.php';
 
 $sentence = new Hello();
-echo $sentence->talk();
+echo $sentence->talk(). PHP_EOL;
+
+
+//$ehime = new \HelloWorld\SayHello();
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+use HelloWorld\SayHello;
+echo SayHello::world();
